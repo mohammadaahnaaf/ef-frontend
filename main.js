438d25c56915e816fd8d5c857fe8600976d3f86a
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-08-19 21:33:04 +0600

    main init

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './home.css'
+// import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <div class="bg">
 <nav>
-    <img class="logo" src="public/logo.webp" />
-    <div class="header-items">
-        <div class="links">
-            <a href="#">Trending</a>
-            <a href="#">Pre-orders</a>
-            <a href="#">upcomming</a>
-            <a href="#">Support 24/7</a>
-        </div>
-        <input class="search" type="search" placeholder="Advanced search" />
+  <img class="logo" src="/logo.webp" />
+  <div class="header-items">
+    <div class="links">
+      <a href="#">Trending</a>
+      <a href="#">Pre-orders</a>
+      <a href="#">upcomming</a>
+      <a href="#">Support 24/7</a>
     </div>
-    <div class="right-icons">
+    <input class="search" type="search" placeholder="Advanced search" />
+  </div>
+  <div class="right-icons">
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="icon">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+      stroke="currentColor" class="icon">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+    </svg>
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="icon">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+      stroke="currentColor" class="icon">
+      <path stroke-linecap="round" stroke-linejoin="round"
+        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+    </svg>
 
-    </div>
+  </div>
 
 </nav>
 <main class="the-main">MAin</main>
@@ -38,5 +36,3 @@ document.querySelector('#app').innerHTML = `
 <footer class="the-footer">footer</footer>
 </div>
 `
-
-// setupCounter(document.querySelector('#counter'))
