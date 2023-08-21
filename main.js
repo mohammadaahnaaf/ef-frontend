3ae36ffc0752fa68b0ee1fc296d2743e8876ce5b
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-08-21 19:33:32 +0600

    feat: :lipstick: little error fix

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector('#app').innerHTML = `
 <div class="bg">
 <nav>
     <img class="logo" src="/logo.webp" />
-
+    <div class="header-itemq"></div>
     <div class="header-items">
         <div class="links">
             <a href="#">Trending</a>
@@ -118,13 +118,15 @@ document.querySelector('#app').innerHTML = `
 
                 </div>
                 <!-- search  -->
-                <button id="searchButton" class="srch-btn">
+                <div>
+                 <button id="searchButton" class="srch-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="menu-icon">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                      stroke="currentColor" class="menu-icon">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
-                </button>
+                 </button>
+                </div>
             </div>
         </div>
 
@@ -437,8 +439,8 @@ Nintendo
     </div>
 
 </div>
-<footer>Ahnafia</footer>
 </div>
+
   `
 
 const searchBox = document.getElementById('searchBox');
